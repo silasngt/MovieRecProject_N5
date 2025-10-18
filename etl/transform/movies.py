6f441fdab9c292c21248cdd9f7664ad23923b994
silas <nguyengiangthanhtai2016@gmail.com>
2025-10-18 21:46:12 +0700

    Bổ sung thêm cấu trúc chung cho file movies và ratings

---
+++ etl/transform/movies.py
@@ -0,0 +1,11 @@
+def clean_movies():
+    """
+    Làm sạch dữ liệu movies.csv
+    - Tách title và year
+    - Chuyển genres thành danh sách
+    """
+    pass
+
+
+if __name__ == "__main__":
+    clean_movies()
---
+++ etl/transform/ratings.py
@@ -0,0 +1,12 @@
+def clean_ratings():
+    """
+    Làm sạch dữ liệu ratings.csv
+    - Chuyển kiểu dữ liệu
+    - Lọc rating hợp lệ
+    - Chuyển timestamp sang datetime
+    """
+    pass
+
+
+if __name__ == "__main__":
+    clean_ratings()
